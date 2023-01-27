e5a90d4c61125e44135f83b28d2a267b8f1db545
Frank Betancur <alejok6@gmail.com>
2023-01-26 21:42:07 -0500

    chore: add environment files

--- src/app/shared/services/raffle.service.ts
+++ src/app/shared/services/raffle.service.ts
@@ -2,24 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { PeopleListResponse, Person } from 'src/app/models/person';
 import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RaffleService {
-  url: string = 'http://localhost:4000/api/v1/raffles';
+  private urlBase = environment.api.url;
 
   constructor(private http: HttpClient) {}
 
   addPerson(person: Person): Observable<any> {
     return this.http
-      .post(`${this.url}/addCompetitor`, person)
+      .post(`${this.urlBase}/addCompetitor`, person)
       .pipe(catchError((error: HttpErrorResponse) => throwError(error)));
   }
 
   getPeopleList(): Observable<any> {
     return this.http
-      .get<PeopleListResponse>(`${this.url}/getWiners`)
+      .get<PeopleListResponse>(`${this.urlBase}/getWiners`)
       .pipe(catchError((error: HttpErrorResponse) => throwError(error)));
   }
 }
---
+++ src/environments/environment.example.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: '',// false for develop, true for prod
+  api: {
+    url: '' // api url
+  }
+};
